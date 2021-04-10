0a3fb09759f7bb70f91920f91aaaa24fbba24f6c
he <he@example.com>
2021-04-10 11:10:16 +0000

    add new path into urls.py

--- app/urls.py
+++ app/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('', views.mainpage),
     path('sightings/', views.list),
     path('sightings/<str:unique_squirrel_id>/', views.edit, name='edit'),
+    path('sightings/add/', views.add, name = 'add'),
+    path('sightings/stats/', views.stats, name = 'stats'),
 ]
