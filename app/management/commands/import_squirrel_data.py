ec28c2cd2449400793e451d385c7993029d483b1
zl2979 <zl2979@columbia.com>
2021-04-12 09:46:19 +0000

    fix minor errors in import.py and import data

--- app/management/commands/import_squirrel_data.py
+++ app/management/commands/import_squirrel_data.py
@@ -2,6 +2,7 @@ from django.core.management.base import BaseCommand
 
 from app.models import Squirrel
 import csv
+import datetime
 
 class Command(BaseCommand):
     
@@ -14,35 +15,42 @@ class Command(BaseCommand):
         with open(options['csv']) as fp:
             data = csv.DictReader(fp)
             
-        for i in data:
-            s = Squirrel(
-                unique_squirrel_id = i['Unique Squirrel ID'],
-                X = i['X'],
-                Y = i['Y'],
-                shift = i['Shift'],
-                date = i['Date'],
-                age = i['Age'],
-                primary_fur_color = i['Primary_Fur_Color'],
-                location = i['Location']
-                specific_location = i['Specific Location']
-                running = str(i['Running']) == 'TRUE'
-                chasing = str(i['Chasing']) == 'TRUE'
-                climbing = str(i['Climbing']) == 'TRUE'
-                eating = str(i['Eating']) == 'TRUE'
-                foraging = str(i['Foraging']) == 'TRUE'
-                other_activity = i['Other Activities']
-                kuks = str(i['Kuks']) == 'TRUE'
-                quaas = str(i['Quaas']) == 'TRUE'
-                moans = str(i['Moans']) == 'TRUE'
-                tail_flags = str(i['Tail flags']) == 'TRUE'
-                tail_twitches = str(i['Tail twitches']) == 'TRUE'
-                approaches = str(i['Approaches']) == 'TRUE'
-                indifferent = str(i['Indifferent']) == 'TRUE'
-                runs_from = str(i['Runs from']) == 'TRUE'
-                )
+            for i in data:
+                day = i['Date']
+                if day:
+                    s_day = day[-4:] + '-' + day[:2] + '-' + day[2:4]
+                    d = datetime.datetime.strptime(s_day, '%Y-%m-%d')
+                else:
+                    d = None
+
+                s = Squirrel(
+                    unique_squirrel_id = i['Unique Squirrel ID'],
+                    X = i['X'],
+                    Y = i['Y'],
+                    shift = i['Shift'],
+                    date = d,
+                    age = i['Age'],
+                    primary_fur_color = i['Primary Fur Color'],
+                    location = i['Location'],
+                    specific_location = i['Specific Location'],
+                    running = (str(i['Running']) == 'TRUE'),
+                    chasing = (str(i['Chasing']) == 'TRUE'),
+                    climbing = (str(i['Climbing']) == 'TRUE'),
+                    eating = (str(i['Eating']) == 'TRUE'),
+                    foraging = (str(i['Foraging']) == 'TRUE'),
+                    other_activities = i['Other Activities'],
+                    kuks = (str(i['Kuks']) == 'TRUE'),
+                    quaas = (str(i['Quaas']) == 'TRUE'),
+                    moans = (str(i['Moans']) == 'TRUE'),
+                    tail_flags = (str(i['Tail flags']) == 'TRUE'),
+                    tail_twitches = (str(i['Tail twitches']) == 'TRUE'),
+                    approaches = (str(i['Approaches']) == 'TRUE'),
+                    indifferent = (str(i['Indifferent']) == 'TRUE'),
+                    runs_from = (str(i['Runs from']) == 'TRUE'),
+                    )
             
-            s.save()
-            self.stdout.write(self.style.SUCCESS('Added member success!'))
+                s.save()
+                #self.stdout.write(self.style.SUCCESS('Added member success!'))
         
         self.stdout.write(self.style.SUCCESS('Successful import all data'))
         
